502f33ba78bcc17a8b91cd9f93294d6ea1c197a9
Akshaya123-git <82938609+Akshaya123-git@users.noreply.github.com>
2021-04-23 01:28:00 +0530

    Update JDBCClass.java

--- JDBCClass.java
+++ JDBCClass.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+SCa
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;

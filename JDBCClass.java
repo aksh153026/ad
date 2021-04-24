935110b5fd8aa5a930675015d44e9690d8a77e84
aksh153026 <57639109+aksh153026@users.noreply.github.com>
2021-04-25 00:38:23 +0530

    Update JDBCClass.java

--- JDBCClass.java
+++ JDBCClass.java
@@ -1,17 +1,14 @@
- package controllers;
 
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-SCa
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 
 public class JDBCClass {
